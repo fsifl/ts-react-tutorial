4ac92e65d14ab1591ce5b1c21e6b0762a2cd8ac5
fsifl <zhantao5858@outlook.com>
2021-08-08 21:40:28 +0800

    02-box children

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
-import Application from './Application';
+//import Application from './Application';
+import Application from "./BoxChildren";
 import { render } from 'react-dom';
 import './style.scss';
 
 const rootElement = document.getElementById("root");
+
 render(<Application />, rootElement);
